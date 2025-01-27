87f503b28f299fbfa838cfa28224d14d02e37b13
Gustavo <40724636+gustavx404@users.noreply.github.com>
2025-01-27 20:49:36 -0300

    update

---
+++ oneBitCode/6-string.py
@@ -0,0 +1,13 @@
+gameName = "Fifa 23"
+gameName2 = "fifa 23"
+print(gameName)
+print(gameName2)
+print(gameName == gameName2) 
+gameDescription = """
+    Fifa 23 e um jogo de futebol
+    desenvolvido pela EA Sports
+    e que possibilita jogar localmente ou online
+"""
+print(gameDescription)
+
+gameName = ""
