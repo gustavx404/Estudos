3cccd62d99f015cb6ab6a8b5525500e950b01c65
gustavx404 <onex404@proton.me>
2025-02-02 12:35:05 -0300

    update onebit ex 7

---
+++ oneBitCode/7-operacoes_strings.py
@@ -0,0 +1,9 @@
+gameDescription = """
+fifa 23 e um jogo de futebol desenvolvido pela EA Sports 
+"""
+gameName = "Fifa"
+gameVersion = " 23"
+# 1 - 
+gameFullName = gameName + gameVersion
+
+print(gameFullName)
