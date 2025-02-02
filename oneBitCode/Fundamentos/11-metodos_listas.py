bfd4efb24371dff8410a9d8dd1ec8a79c517a47d
gustavx404 <onex404@proton.me>
2025-02-02 15:41:31 -0300

    aprendendo tuplas

--- oneBitCode/Fundamentos/11-metodos_listas.py
+++ oneBitCode/Fundamentos/11-metodos_listas.py
@@ -1,3 +1,25 @@
 gameList = ["Resident Evil 4", "Star Wars Jedi Survivor",
             "The legend of zelda", "Red DeaD 2"]
 
+# 1 - Tamanho da lista
+print(len(gameList))
+
+# 2 Recuperar um item da lista pelo indece
+print(gameList.index("Resident Evil 4"))
+
+# 3 - Adicionar item na lista
+gameList.append("GTA V")
+print(gameList)
+
+# 4 - Ordenar lista
+gameList.sort()
+print(gameList)
+
+# 5 - Copiar item de uma lista pra outra
+gameReset = gameList.copy()
+gameReset.remove("Red DeaD 2")
+print(gameReset)
+
+# 6 - Remover tudo da lista
+gameList.clear()
+print(gameList)
---
+++ oneBitCode/Fundamentos/12-tuplas.py
@@ -0,0 +1,6 @@
+gamesTuple = ("Fifa23", "Red Dead 2", "Star Wars"
+              "Mario Odyssey", "The Legend of Zelda")
+print(gamesTuple)
+
+# Tupla nao pode ser  removido valores nem passado valores e nem ordenar valores
+
