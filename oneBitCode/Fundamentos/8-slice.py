d33c7841988eb3337698bad088dcb96d9b9750de
gustavx404 <onex404@proton.me>
2025-02-02 12:56:22 -0300

    ex 8 slice string

--- oneBitCode/Fundamentos/8-slice.py
+++ oneBitCode/Fundamentos/8-slice.py
@@ -16,3 +16,15 @@ print(gameName[:5])
 # 3 - Busque toda string da terceira ate a ultima posicao
 
 print(gameName[2:])
+
+# 3 - Busque toda string de 2 em 2 caracteres
+
+print(gameName[::2])
+
+# 4 - Busque toda string nos indeces impares
+
+print(gameName[1::2])
+
+# 5 - Inverter uma string de tras pra frente
+
+print(gameName[::-1])
