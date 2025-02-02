a3e79108fa65afcc479ad76abdf109ce6fe3d427
gustavx404 <onex404@proton.me>
2025-02-02 15:12:55 -0300

    aprendendo listas python

---
+++ oneBitCode/Fundamentos/10-listas.py
@@ -0,0 +1,18 @@
+gameFifa = ["Fifa 23", 2022, 90.50, True]
+print(gameFifa)
+
+gameList = ["Resident Evil 4", "Star Wars Jedi Survivor",
+            "The legend of zelda", "Red DeaD 2"]
+print(gameList)
+
+# 1 - Buscar os dois primeiros itens da lista
+print(gameList[:2])
+
+# 2 - Buscar o ultimo item da lista
+print(gameList[-1])
+
+# 3 - Buscar jogos ate uma determinada posicao
+print(gameList[:3])
+
+# 4 - buscar jogos de uma posicao em diante
+print(gameList[1:4])
---
+++ oneBitCode/Fundamentos/11-metodos_listas.py
@@ -0,0 +1,3 @@
+gameList = ["Resident Evil 4", "Star Wars Jedi Survivor",
+            "The legend of zelda", "Red DeaD 2"]
+
